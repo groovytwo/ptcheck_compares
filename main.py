47c7f66d07b0de270ad6fdb8dffc488c4062227c
Adam Ellery <adam.d.ellery@gmail.com>
2023-02-01 16:02:11 -0700

    Experimentation with merging dataframes

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
-from my_dataframe import test_df
+from my_dataframe import CreateDictDataframe
 from my_initialize import ConfigParser
+
+def dataframe_difference(df1, df2, which=None):
+    """Find rows which are different between two DataFrames."""
+    comparison_df = df1.merge(
+        df2,
+        indicator=True,
+        how='outer'
+    )
+    if which is None:
+        diff_df = comparison_df[comparison_df['_merge'] != 'both']
+    else:
+        diff_df = comparison_df[comparison_df['_merge'] == which]
+    diff_df.to_csv('data/diff.csv')
+    return diff_df
 
 cfg = ConfigParser()
 
-sheet1 = test_df(cfg, cfg.file.ptcheck1_path)
+sheet1 = CreateDictDataframe(cfg, cfg.file.ptcheck1_path)
+sheet2 = CreateDictDataframe(cfg, cfg.file.ptcheck2_path)
 
+df1 = sheet1.dict_df['Analog']
+df2 = sheet1.dict_df['Analog']
+
+check = df1.compare(df2)
+
+check = dataframe_difference(df1, df2)
+
+
+# https://www.statology.org/pandas-can-only-compare-identically-labeled-series-objects/
 print("Ending")
 
--- my_dataframe.py
+++ my_dataframe.py
@@ -1,4 +1,5 @@
 import re
+
 import pandas as pd
 
 
@@ -12,37 +13,40 @@ def parse_remote_info(dict_df):
     remote_desc = remote_string.group(2)
     return remote_name, remote_desc
 
-def test_df(cfg, file_path):
-    dict_df = pd.read_excel(file_path, sheet_name=cfg.dataframe.names_list)
 
-    # Get the first dictionary value from dict_df
-    remote_name, remote_desc = parse_remote_info(dict_df)
+class CreateDictDataframe:
+    def __init__(self, cfg, file_path):
+        self.dict_df = pd.read_excel(file_path, sheet_name=cfg.dataframe.names_list)
+        self.parse_remote_info()
+        for df_cfg in cfg.dataframe.df_list:
+            df_cfg = getattr(cfg, df_cfg)
+            df = self.dict_df[df_cfg.name]
 
-
-    # Lookup the Remote Name & Description
-
-    for df_cfg in cfg.dataframe.df_list:
-        df_cfg = getattr(cfg, df_cfg)
-        df = dict_df[df_cfg.name]
-
-        # Drop first four rows
-        df = df.drop(df.index[0:3])
-        # Drop rows after Description
-        df = df.drop(df.index[1:4])
-        # Drop empty columns
-        df = df.drop(df.columns[df_cfg.delete_col], axis=1)
-        # Assign column names from first row
-        df.columns = df.iloc[0]
-        # Drop first row that was used to name columns in previous step
-        df.drop(index=df.index[0], axis=0, inplace=True)
-        # Drop rows that are all 'nan'
-        df = df.dropna(axis = 0, how = 'all')
-        df.reset_index(drop=True, inplace=True)
-
-        if "defined" in (df.iat[0, 0]): # If dataframe is empty
-            # Delete row that says points are not defined
+            # Drop first four rows
+            df = df.drop(df.index[0:3])
+            # Drop rows after Description
+            df = df.drop(df.index[1:4])
+            # Drop empty columns
+            df = df.drop(df.columns[df_cfg.delete_col], axis=1)
+            # Assign column names from first row
+            df.columns = df.iloc[0]
+            # Drop first row that was used to name columns in previous step
             df.drop(index=df.index[0], axis=0, inplace=True)
+            # Drop rows that are all 'nan'
+            df = df.dropna(axis=0, how='all')
+            df.reset_index(drop=True, inplace=True)
 
-        dict_df[df_cfg.name] = df
-    return dict_df
+            if "defined" in (df.iat[0, 0]):  # If dataframe is empty
+                # Delete row that says points are not defined
+                df.drop(index=df.index[0], axis=0, inplace=True)
 
+            self.dict_df[df_cfg.name] = df
+
+    def parse_remote_info(self):
+        # Get the first dictionary value from dict_df
+        first_key = list(self.dict_df.keys())[0]
+        # Fetch remote info string (including name + desc)
+        remote_info = self.dict_df[first_key].iat[5, 0]
+        remote_string = re.match("(.*?)\s-\s(.*?)\s\((.*)\)", remote_info)
+        self.remote_name = remote_string.group(1)
+        self.remote_desc = remote_string.group(2)
