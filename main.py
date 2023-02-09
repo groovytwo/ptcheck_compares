7df9297d84d1c73c71828459e49c63f74539ae3c
Adam Ellery <adam.d.ellery@gmail.com>
2023-02-09 15:09:50 -0700

    Added capability to reverse flow

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ import easygui as gui
 
 cfg = ConfigParser()
 
+
+message = f"Are you copying from {cfg.file.ptcheck_old_tag} to {cfg.file.ptcheck_new_tag}?"
+title = "Direction of flow"
+cfg.normal_flow = gui.ynbox(message, title)
+
 open1_title = f"Select {cfg.file.ptcheck_old_tag} Excel file to compare."
 file1_type = "*.xlsx"
 file1_path = gui.fileopenbox(title=open1_title, default=fr"{cfg.file.ptcheck_old_path}")
@@ -15,10 +20,14 @@ open2_title = f"Select {cfg.file.ptcheck_new_tag} Excel file to compare."
 file2_type = "*.xlsx"
 file2_path = gui.fileopenbox(title=open2_title, default=fr"{cfg.file.ptcheck_new_path}")
 
+
 sheet1 = CreateDictDataframe(cfg, file1_path)
 sheet2 = CreateDictDataframe(cfg, file2_path)
 
-diff_df = CreateDiffDataframe(sheet2, sheet1, cfg)
+if cfg.normal_flow:
+	diff_df = CreateDiffDataframe(sheet2, sheet1, cfg)
+else:
+	diff_df = CreateDiffDataframe(sheet1, sheet2, cfg)
 
 CreateExcel(diff_df.df, cfg)
 
